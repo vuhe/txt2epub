144af1878a9c2acec234606c48c578ee9755e864
vuhe <zhuhe202@qq.com>
2024-06-22 19:43:40 +0800

    fix: 生成封面参数错误

--- src/orly_cover.rs
+++ src/orly_cover.rs
@@ -2,9 +2,9 @@ use anyhow::Result;
 use reqwest::blocking::Client;
 
 static COLORS: [&str; 17] = [
-    "#61005e", "#70706d", "#890029", "#c4000e", "#6d001d", "#6a00bd", "#f10000", "#0071b1",
-    "#f9bc00", "#2c0077", "#ba009a", "#009047", "#009d9e", "#222e85", "#bd002e", "#009d1a",
-    "#75a500",
+    "61005e", "70706d", "890029", "c4000e", "6d001d", "6a00bd", "f10000", "0071b1",
+    "f9bc00", "2c0077", "ba009a", "009047", "009d9e", "222e85", "bd002e", "009d1a",
+    "75a500",
 ];
 
 pub fn gen_orly_cover(title: &str, author: &str, color: u8, img: u8) -> Result<Vec<u8>> {
